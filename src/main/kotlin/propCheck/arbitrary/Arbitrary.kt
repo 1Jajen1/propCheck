b6c766755f3846fa802489c0c6d14be1764ea423
Jannis <overesch.jannis@gmail.com>
2019-10-25 00:36:22 +0200

    Improve shrinking lists with lazyness (#16)
    * Very ugly way to introduce some more lazyness to shrinkList
    
    * Fix a nasty bug with shrinking integrals
    
    * Give Function test more freedom

--- src/main/kotlin/propCheck/arbitrary/Arbitrary.kt
+++ src/main/kotlin/propCheck/arbitrary/Arbitrary.kt
@@ -125,12 +125,12 @@ fun <A> shrinkList(list: List<A>, f: (A) -> Sequence<A>): Sequence<List<A>> {
     fun <F> removes(k: Int, n: Int, l: List<F>): Sequence<List<F>> =
         if (k > n) emptySequence()
         else if (l.isEmpty()) sequenceOf(emptyList())
-        else sequenceOf(l.drop(k)) + removes(k, (n - k), l.drop(k)).map { l.take(k) + it }
+        else sequenceOf(l.drop(k)) + sequenceOf(Unit).flatMap { removes(k, (n - k), l.drop(k)).map { l.take(k) + it } }
 
     fun shrinkListIt(l: List<A>): Sequence<List<A>> = when (l.size) {
         0 -> emptySequence()
         else -> iterate({ it / 2 }, l.size).takeWhile { it > 0 }
-            .map { k -> removes(k, l.size, l) }.reduce { acc, list -> acc + list }
+            .map { k -> removes(k, l.size, l) }.reduce { a, b -> a + b }
     }
 
     fun shrinkOne(l: List<A>): Sequence<List<A>> = when (l.size) {
@@ -140,43 +140,14 @@ fun <A> shrinkList(list: List<A>, f: (A) -> Sequence<A>): Sequence<List<A>> {
     }
 
     return if (list.isEmpty()) emptySequence()
-    else shrinkListIt(list) + shrinkOne(list)
+    else shrinkListIt(list) + sequenceOf(Unit).flatMap { shrinkOne(list) }
 }
 
 // ---------------------------- Number shrinkers
 fun shrinkByte(fail: Byte): Sequence<Byte> = (
         (if (fail < 0 && (-fail).toByte() > fail) sequenceOf(-fail) else emptySequence()) +
                 (sequenceOf(0) + iterate({ it / 2 }, fail.toInt()).drop(1)
-                    .map { fail - it }.takeWhile {
-                        when ((it >= 0) toT (fail >= 0)) {
-                            Tuple2(true, true) -> it < fail
-                            Tuple2(false, false) -> it > fail
-                            Tuple2(true, false) -> (it + fail) < 0
-                            Tuple2(false, true) -> (it + fail) > 0
-                            else -> throw IllegalStateException("The impossible happened")
-                        }
-                    }
-                        )
-        ).map { it.toByte() } // safe because shrinking only makes things smaller and we start with a byte anyway
-
-fun shrinkInt(fail: Int): Sequence<Int> = (
-        (if (fail < 0 && -fail > fail) sequenceOf(-fail) else emptySequence()) +
-                (sequenceOf(0) + iterate({ it / 2 }, fail).drop(1)
-                    .map { fail - it }.takeWhile {
-                        when ((it >= 0) toT (fail >= 0)) {
-                            Tuple2(true, true) -> it < fail
-                            Tuple2(false, false) -> it > fail
-                            Tuple2(true, false) -> (it + fail) < 0
-                            Tuple2(false, true) -> (it + fail) > 0
-                            else -> throw IllegalStateException("The impossible happened")
-                        }
-                    }
-                        )
-        )
-
-fun shrinkLong(fail: Long): Sequence<Long> = (
-        (if (fail < 0 && -fail > fail) sequenceOf(-fail) else emptySequence()) +
-                (sequenceOf(0L) + iterate({ it / 2 }, fail).drop(1).map { fail - it }.takeWhile {
+                    .map { fail - it }).takeWhile {
                     when ((it >= 0) toT (fail >= 0)) {
                         Tuple2(true, true) -> it < fail
                         Tuple2(false, false) -> it > fail
@@ -185,7 +156,33 @@ fun shrinkLong(fail: Long): Sequence<Long> = (
                         else -> throw IllegalStateException("The impossible happened")
                     }
                 }
-                        )
+        ).map { it.toByte() } // safe because shrinking only makes things smaller and we start with a byte anyway
+
+fun shrinkInt(fail: Int): Sequence<Int> = (
+        (if (fail < 0 && -fail > fail) sequenceOf(-fail) else emptySequence()) +
+                (sequenceOf(0) + iterate({ it / 2 }, fail).drop(1)
+                    .map { fail - it }).takeWhile {
+                    when ((it >= 0) toT (fail >= 0)) {
+                        Tuple2(true, true) -> it < fail
+                        Tuple2(false, false) -> it > fail
+                        Tuple2(true, false) -> (it + fail) < 0
+                        Tuple2(false, true) -> (it + fail) > 0
+                        else -> throw IllegalStateException("The impossible happened")
+                    }
+                }
+        )
+
+fun shrinkLong(fail: Long): Sequence<Long> = (
+        (if (fail < 0 && -fail > fail) sequenceOf(-fail) else emptySequence()) +
+                (sequenceOf(0L) + iterate({ it / 2 }, fail).drop(1).map { fail - it }).takeWhile {
+                    when ((it >= 0) toT (fail >= 0)) {
+                        Tuple2(true, true) -> it < fail
+                        Tuple2(false, false) -> it > fail
+                        Tuple2(true, false) -> (it + fail) < 0
+                        Tuple2(false, true) -> (it + fail) > 0
+                        else -> throw IllegalStateException("The impossible happened")
+                    }
+                }
         )
 
 fun shrinkFloat(fail: Float): Sequence<Float> = (
